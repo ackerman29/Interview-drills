2e2fd92c76948b57eeae822e6340f7091fb18686
Rupanjan Pal <rupanjanpal54@gmail.com>
2025-09-13 11:31:15 +0530

    everything working don't touch (1)

--- k6_test.js
+++ k6_test.js
@@ -1,16 +1,16 @@
-import http from "k6/http";
-import { check, sleep } from "k6";
+const http = require("k6/http");
+const { check, sleep } = require("k6");
 
-export let options = {
+exports.options = {
   vus: 10,         // 10 virtual users
   duration: "10s", // 10 seconds
 };
 
-export default function () {
+exports.default = function () {
   const res = http.get("http://localhost:4000/api/drills"); // your route
   check(res, {
     "status is 200": (r) => r.status === 200,
     "response time < 500ms": (r) => r.timings.duration < 500
   });
   sleep(1);
-}
+};
